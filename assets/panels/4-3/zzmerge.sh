64cff968b1b034938825c9f73f0433d79ccb33f8
Dave Marsdin <davemarsdin@gmx.com>
2026-03-27 15:05:11 +0000

    New systems for 4.9 Note that wallpaper and panel images are placeholders

--- assets/panels/4-3/zzmerge.sh
+++ assets/panels/4-3/zzmerge.sh
@@ -4,9 +4,14 @@ until [ $counter -gt 250 ]
 do
 echo -n "System : "
 read filename
-    convert  "${filename}.svg" \( +clone -background black -shadow 100x2+4+4 \) +swap \           -background none  -layers merge +repage  "${filename}.png"
-    convert "${filename}.jpg" "${filename}.png" -gravity center -compose over -composite "${filename}.jpg"
-rm ${filename}.svg
-rm ${filename}.png
+    convert "${filename}.jpg" -resize 640x480^ -gravity center -extent 640x480 "${filename}.jpg"
+    inkscape -w 500 -h 200 "${filename}.svg" -o "${filename}.png"
+    convert  "${filename}.png" \( +clone -background black -shadow 100x2+4+4 \) +swap \           -background none  -layers merge +repage  "${filename}b.png"
+    convert "${filename}.jpg" "${filename}b.png" -gravity center -compose over -composite "${filename}b.jpg"
+rm "${filename}.png"
+rm "${filename}b.png"
+rm "${filename}.jpg"
+mv "${filename}b.jpg" "${filename}.jpg"
+rm "${filename}.svg"
  ((counter++))
 done
